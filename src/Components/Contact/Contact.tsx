d4ce08878d64c673228b4e8d06ed120fd6e9fc7c
KamilCodeDev <akadel.76@gmail.com>
2024-10-13 11:17:54 +0500

    img

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,12 +1,13 @@
 import { useContact } from './hook/useContact';
 import styles from './Contact.module.scss';
 import { PiPhoneCall } from 'react-icons/pi';
-import telegram from '../../assets/telegram.png';
-import intagram from '../../assets/instagram.png';
-import facebook from '../../assets/facebook (2).png';
 import { MdOutlineMailOutline } from 'react-icons/md';
 import { useTranslation } from 'react-i18next';
-
+import Telegram from "../../assets/telegram.png"
+import instagram from "../../assets/instagram.png"
+import facebook from "../../assets/facebook (2).png"
+import email from "../../assets/email (2).png"
+import call from "../../assets/calling.png"
 const Contact = () => {
   const { data: contact } = useContact();
   const { t } = useTranslation();
@@ -19,7 +20,7 @@ const Contact = () => {
         {contact?.map((contact) => (
           <div key={contact.id} className={styles.contactWrapper}>
             <div>
-              <PiPhoneCall size={30} />
+              <img src={call} style={{ width: '30px', height: '30px' }} alt="" />
               <a
                 href={`tel:${contact.first_contact}`}
                 className={styles.contactItem}
@@ -31,14 +32,14 @@ const Contact = () => {
         ))}
         <div className={styles.info}>
           <div className={styles.email}>
-            <MdOutlineMailOutline size={34} />
+            <img src={email} style={{ width: '30px', height: '30px' }} alt="" />
             <p>office@seyyam.net</p>
           </div>
           <div className={styles.logo}>
             <a href='https://www.instagram.com/seyyam__group/' target='_blank'>
-              <img src={intagram} alt='' />
+              <img src={instagram} alt='' />
             </a>
-            <img src={telegram} alt='' />
+            <img src={Telegram} alt='' />
             <img src={facebook} alt='' />
           </div>
         </div>
